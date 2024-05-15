956dec017d6e306d5dc7005074afc26f50fcbcd0
daw223-14 <jimmy.almaquio.cespedes@iesjulianmarias.es>
2024-05-15 20:12:50 +0200

    Dockerizado prueba3.2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   base: '/',
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 80,
     strictPort: true,
     host: '0.0.0.0', // Configura la dirección IP de tu servidor aquí
   },
