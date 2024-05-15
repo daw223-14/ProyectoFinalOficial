b5231debdcccdbafbc02b38da25c7b0e4b2d330d
daw223-14 <jimmy.almaquio.cespedes@iesjulianmarias.es>
2024-05-15 20:15:54 +0200

    Dockerizado prueba3.2

--- frontend/src/components/axios.jsx
+++ frontend/src/components/axios.jsx
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
     //baseURL: 'http://localhost:80'
-    baseURL: 'http://200.234.236.135'
+    baseURL: 'http://200.234.236.135:8080'
 });
